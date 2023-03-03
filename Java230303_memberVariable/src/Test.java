7abc9f1e89efbd9c9fc984cf97a1426bc0303f32
Lainari <108247620+Lainari@users.noreply.github.com>
2023-03-03 17:47:30 +0900

    Create Test.java

---
+++ Java230303_memberVariable/src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        JavaTest t1 = new JavaTest();
+        t1.setName("HongGilDong");
+        System.out.println(t1.getName());
+    }
+}
