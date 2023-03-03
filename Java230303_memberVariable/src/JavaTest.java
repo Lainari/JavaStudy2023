9016b27ec61067fe61f34f53287dcd51f951485d
Lainari <108247620+Lainari@users.noreply.github.com>
2023-03-03 17:47:27 +0900

    Create JavaTest.java

---
+++ Java230303_memberVariable/src/JavaTest.java
@@ -0,0 +1,22 @@
+public class JavaTest {
+    private String name;
+
+    String getName(){
+        return name;
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
+
+    double square(double df){
+        test();
+        return df * df;
+    }
+    int square(int num){
+        return num * num;
+    }
+
+    void test(){
+    }
+}
