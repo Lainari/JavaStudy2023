d02a24ea79cdaabb289105d1e2e7d5fbfc512823
Lainari <108247620+Lainari@users.noreply.github.com>
2023-03-09 11:23:34 +0900

    Update JavaTest.java

--- Java230309_ClassTime/src/JavaTest.java
+++ Java230309_ClassTime/src/JavaTest.java
@@ -13,6 +13,7 @@ public class JavaTest {
         System.out.println("Hiiiiiii");
     }
 
+    // instance method
     void setName(String name){
         // 맴버 메서드의 매개변수와 맴버 변수와 이름이 같은 경우, 맴버 변수의 우선순위가 높음
         // 따라서 this 연산자를 사용한다
@@ -24,7 +25,10 @@ public class JavaTest {
 
         t.name = "HongGilDong";
         t.setName(t.name);
+        
+        // instance 마다 다른 객체나 매소드로 구현해야한다면 instance 로 구현해야한다
+        // 그럴 필요가 없다면 static 으로 구현하기
 
-        JavaTest.sayHello(); // static method는 따로 instance로 만들지 않아도 실행이 가능함
+        JavaTest.sayHello(); // static method 는 따로 instance 로 만들지 않아도 실행이 가능함
     }
 }
