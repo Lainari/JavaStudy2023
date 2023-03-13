1385645088c4d6da4e0b9b2fa42c5024dd3a926a
Lainari <108247620+Lainari@users.noreply.github.com>
2023-03-13 20:14:30 +0900

    Update PersonTest.java

--- Java230313_ClassTime/src/test/PersonTest.java
+++ Java230313_ClassTime/src/test/PersonTest.java
@@ -3,8 +3,11 @@ package test;
 public class PersonTest {
     public static void main(String[] args) {
         Person p = new Person();
-        p.setAge(-20);
+        p.setAge(15);
         System.out.println(p.getAge());
+
+        System.out.println(p.getName());
+        System.out.println(p.getDept());
 
         // 만약, Person 클래스에서 age 변수가 private 이 아니라면 아래의 코드가 동작 가능하다
         // p.age = -200
