b8dde9dab79428bf94da2b784e093793baf6f006
Lainari <108247620+Lainari@users.noreply.github.com>
2023-03-13 20:01:27 +0900

    Update Person.java

--- Java230313_ClassTime/src/test/Person.java
+++ Java230313_ClassTime/src/test/Person.java
@@ -2,19 +2,19 @@ package test;
 
 public class Person {
     private String name;   // 멤버 변수는 거의 무조건 private 로 숨겨놓자!
-    private int age;
+    private int myAge;
 
     public String getName(){
         return name;       // private 멤버 변수는 public 메소드로 접근할 수 있도록 해주자
     }
 
     public int getAge(){
-        return age;
+        return myAge;
     }
 
-    public void setAge(int age){
-        if(age >= 0 && age <= 150)
-          this.age = age;
+    public void setAge(int argAge){
+        if(argAge >= 0 && argAge <= 150)
+          this.myAge = argAge;
         System.out.println("나이의 범위는 0부터 150까지 입니다.");
     }
 
