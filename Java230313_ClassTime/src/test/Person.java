3e09f96c6f9c67d36337bd700cb828b7341ae9a3
Lainari <108247620+Lainari@users.noreply.github.com>
2023-03-13 20:02:31 +0900

    Update Person.java

--- Java230313_ClassTime/src/test/Person.java
+++ Java230313_ClassTime/src/test/Person.java
@@ -3,7 +3,8 @@ package test;
 public class Person {
     private String name;   // 멤버 변수는 거의 무조건 private 로 숨겨놓자!
     private int myAge;
-
+    // private 로 숨겨놓은 멤버 변수는 이름을 바꾼다면 이 클래스에서만 다 바꾸면 된다.
+    // 따로 호출한 클래스에서 변수 이름을 바꿀 필요가 없어서 훨씬 더 편하다
     public String getName(){
         return name;       // private 멤버 변수는 public 메소드로 접근할 수 있도록 해주자
     }
