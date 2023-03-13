d67c14a522763f9c585be7a557fe3f0744e06495
Lainari <108247620+Lainari@users.noreply.github.com>
2023-03-13 20:00:01 +0900

    Update PersonTest.java

--- Java230313_ClassTime/src/test/PersonTest.java
+++ Java230313_ClassTime/src/test/PersonTest.java
@@ -3,7 +3,7 @@ package test;
 public class PersonTest {
     public static void main(String[] args) {
         Person p = new Person();
-        p.setAge(20);
+        p.setAge(-20);
         System.out.println(p.getAge());
     }
 }
