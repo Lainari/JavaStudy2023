6b653622930366703a0b386ffbca8aabbe8c99ea
Lainari <108247620+Lainari@users.noreply.github.com>
2023-03-13 20:26:49 +0900

    Update PersonTest.java

--- Java230313_ClassTime/src/test/PersonTest.java
+++ Java230313_ClassTime/src/test/PersonTest.java
@@ -8,6 +8,8 @@ public class PersonTest {
 
         System.out.println(p.getName());
         System.out.println(p.getDept());
+        String d = p.dept2; // protected 는 같은 패키지에서 접근이 가능함
+        System.out.println(d);
 
         // 만약, Person 클래스에서 age 변수가 private 이 아니라면 아래의 코드가 동작 가능하다
         // p.age = -200
