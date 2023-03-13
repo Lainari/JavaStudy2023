bf750858f89908d7f00ecf8552057d4442bf6bbc
Lainari <108247620+Lainari@users.noreply.github.com>
2023-03-13 19:55:55 +0900

    Create Test.java

---
+++ Java230313_ClassTime/src/test2/Test.java
@@ -0,0 +1,10 @@
+package test2;
+import test.Person;
+public class Test {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setAge(15);
+        System.out.println(p.getAge());
+    }
+}
+
